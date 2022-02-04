eab7cd10d3ab0c72032ee648ff6f23ca15ab926c
Brian Emmanuel Hernández <tprog.brian.hernandez@gmail.com>
2022-02-04 15:53:12 -0600

    Clase 04/02/22

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace Léxico_0
+{
+    public class Program{
+        
+        static void Main(string[] args){
+
+
+            Console.WriteLine("Hola mundo");
+            /*
+            archivo a = new archivo();
+            //a.desplegar();
+            //a.Palabras();
+            
+            while(!a.FinArchivo()){
+                a.NextToken();
+            }
+            
+            a.Cerrar();*/
+        }
+    }
+}
